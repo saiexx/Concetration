a7c30b5ba46ddc7225b5b392857b6a33b6d66a1f
gatakiriba@hotmail.co.th <gatakiriba@hotmail.co.th>
2019-03-07 15:47:59 +0700

    Initial Commit

---
+++ Concetration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concetration
+//
+//  Created by Sai-Mork on 7/3/19.
+//  Copyright © 2019 Kasidid Wachirachai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
